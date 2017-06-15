c87f3099ac91e7a35a2356f011c9e70e1e7848b1
gb.choi <mosfet1kg@gmail.com>
2017-06-15 22:28:41 +0900

    example cde for createLoginKey was added

--- example/compute/loginkey/index.js
+++ example/compute/loginkey/index.js
@@ -20,4 +20,18 @@ var ncloud = require('../../../lib/');
   });
 
 
+  client.compute.createLoginKey( { keyName: "myKey" }, function( err, response ){
+    if ( err ){
+      console.log( err );
+    } else {
+      console.log( response );
+      //    example=>
+      //   { privateKey: '-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEAs0fJZJWRmxhlYoGCEgyQGtFa6l5dB1N+fZtCqu5XnSUSx/2
+      //   p\nYd7wfjsBp1zINIom9vrALr+qzGsBRT0tCSSpULf6SpQJn4hHqkvqhQ8NoBEyLDLW\n+VmZTBkqBm23ZxKlQ+syS2u56j7ntwm+arZ46k7P9
+      //   Zbxb3hR4Lr1oPVK6IED30+B\ntVgt3tqZSCChIhUEcNfKuedHeBU0bCLM3IED4d4H7JXWlgBO2EXuAP\n9tB0GQKBgQDKCWBqk2gcHM6heQ1Ey
+      //           /** the last part omitted **/
+      //   hcRQVDS1SSiJQCg==\n-----END RSA PRIVATE KEY-----\n' }
+    }
+  })
+
 })();
