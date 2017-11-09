379bb879fd172966d39ce9a7c48dac414af69d58
gb.choi <mosfet1kg@gmail.com>
2017-11-09 23:10:34 +0900

    Update test env

--- __tests__/compute.test.js
+++ __tests__/compute.test.js
@@ -5,23 +5,27 @@ const client = ncloud.createClient(env);
 
 describe('Compute ACG Test', function( ){
   test('ACG API Test #1', function ( done ){
+
     client.compute.findAccessControlGroups( function( error, response ) {
-      if ( error ) {
-        return console.log( error );
+      try {
+        expect( error ).toBeNull();
+
+        expect( response ).toBeDefined();
+
+        const filtered = response.filter({accessControlGroupName: 'ncloud-default-acg'})[0];
+        const keys = Object.keys( filtered );
+
+        expect( keys ).toContain('accessControlGroupConfigurationNo');
+        expect( keys ).toContain('accessControlGroupName');
+        expect( keys ).toContain('accessControlGroupDescription');
+        expect( keys ).toContain('isDefault');
+        expect( keys ).toContain('createDate');
+
+        done();
+
+      } catch (e) {
+        done.fail(e);
       }
-
-      expect( response ).toBeDefined();
-
-      const filtered = response.filter({accessControlGroupName: 'ncloud-default-acg'})[0];
-      const keys = Object.keys( filtered );
-
-      expect( keys ).toContain('accessControlGroupConfigurationNo');
-      expect( keys ).toContain('accessControlGroupName');
-      expect( keys ).toContain('accessControlGroupDescription');
-      expect( keys ).toContain('isDefault');
-      expect( keys ).toContain('createDate');
-
-      done();
     })
   });
 
--- __tests__/geolocation.test.js
+++ __tests__/geolocation.test.js
@@ -6,37 +6,41 @@ const client = ncloud.createClient(env);
 describe('Geolocation API Test', function () {
   test('Geolocation API Test #1', function (done) {
     client.openapi.geolocation.findLocation({ ip: '143.248.142.77'}, function (err, res){
-      if ( err ) {
-        return console.log( err );
-      }
+      try {
+        expect( err ).toBeNull();
 
-      console.log( res );
-      expect( res ).toBeDefined();
-      expect( Object.keys(res) ).toContain('country');
-      expect( Object.keys(res) ).toContain('code');
-      expect( Object.keys(res) ).toContain('r1');
-      expect( Object.keys(res) ).toContain('r2');
-      done();
+        expect( res ).toBeDefined();
+        expect( Object.keys(res) ).toContain('country');
+        expect( Object.keys(res) ).toContain('code');
+        expect( Object.keys(res) ).toContain('r1');
+        expect( Object.keys(res) ).toContain('r2');
+        done();
+      } catch (e) {
+        done.fail( e );
+      }
     })
   });
 
   test('Geolocation API Test #2', function (done) {
     client.openapi.geolocation.findLocation({ ip: '143.248.142.77', ext: 't'}, function (err, res) {
-      if ( err ) {
-        return console.log( err );
-      }
-      console.log(res);
-      expect(res).toBeDefined();
+      try {
+        expect( err ).toBeNull();
 
-      const keys = Object.keys(res);
-      expect(keys).toContain('country');
-      expect(keys).toContain('code');
-      expect(keys).toContain('r1');
-      expect(keys).toContain('r2');
-      expect(keys).toContain('lat');
-      expect(keys).toContain('long');
-      expect(keys).toContain('net');
-      done();
+        expect(res).toBeDefined();
+
+        const keys = Object.keys(res);
+        expect(keys).toContain('country');
+        expect(keys).toContain('code');
+        expect(keys).toContain('r1');
+        expect(keys).toContain('r2');
+        expect(keys).toContain('lat');
+        expect(keys).toContain('long');
+        expect(keys).toContain('net');
+        done();
+
+      } catch (e) {
+        done.fail( e );
+      }
     })
   });
 });
